3fa5e270f6a83dc4f305422dc57a09bf76eb991a
Nazariy <nazarroot3113@gmail.com>
2020-12-25 19:36:51 +0200

    added footer and main amination

--- #src/js/index.js
+++ #src/js/index.js
@@ -1,3 +1,4 @@
+import gsap from 'gsap';
 import webp from './modules/webp';
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -25,4 +26,25 @@ window.addEventListener('DOMContentLoaded', () => {
       headerHide.classList.remove('header__hide--visible');
     }
   });
+  const tl = gsap.timeline({ defaults: { ease: 'power1.out' } });
+  tl.fromTo('.header__logo', {
+    top: '50vh', left: '50vw', x: '-100%', y: '-50%', width: '225px', height: '220px',
+  }, {
+    top: '50vh', left: '50vw', x: '-50%', y: '-50%', width: '225px', height: '220px',
+  });
+  tl.to('.header__logo-letterN', { marginLeft: '42px' }, '-=1');
+  tl.to('.header__logo-letterY', { marginLeft: '-110px' }, '-=1');
+  tl.fromTo('.header__logo-letter', { y: '100%', opacity: 0, fontSize: '130px' }, {
+    y: '0%', opacity: 1, fontSize: '130px', duration: 1,
+  });
+  tl.to('.header__logo', {
+    top: '20px', left: '4rem', x: '0%', y: '0%', width: '50px', height: '44px', duration: 1.3,
+  });
+  tl.fromTo('.header__logo-letter', { fontSize: '130px' }, {
+    fontSize: '26px', duration: 1.3,
+  }, '-=1.3');
+  tl.to('.header__logo-letterN', { margin: '0', duration: 1.3 }, '-=1.3');
+  tl.to('.header__logo-letterY', { marginLeft: '-22px', duration: 1.3 }, '-=1.3');
+  tl.to('.animBg', { opacity: 0, duration: 1 });
+  tl.to('.animBg', { y: '-100%' });
 });
